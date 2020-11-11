665912a850703070b225dd76b23d549fcc7e7ce7
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-11 12:15:35 -0500

    Routes created. Restaurant controller created. Index and show working.

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant = Restaurant.new(params.require(:restaurant).permit(:name, :address, :phone_number, :category))
+    restaurant.save
+
+    redirect_to root_path
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
