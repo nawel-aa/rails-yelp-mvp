c292b2c5a0776fda26c3d906e9352b3773dbd0b2
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-11 13:55:34 -0500

    Reviews in place

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(params.require(:review).permit(:rating, :content))
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
 
-  resources :restaurants, only: %i[new create index show] do 
+  resources :restaurants, only: %i[new create show] do
     resources :reviews, only: %i[new create]
   end
 end
